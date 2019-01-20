d178b92c0be28d3aff9e580c3fdb16bd4f1d7178
Berlin Sky <pedroyoung8989@gmail.com>
2019-01-20 11:16:53 -0800

    add scss

--- stories/index.stories.js
+++ stories/index.stories.js
@@ -1,6 +1,8 @@
 import { document, console } from 'global';
 import { storiesOf } from '@storybook/html';
 
+import './main.scss';
+
 storiesOf('Demo', module)
   .add('heading', () => '<h1>Hello World</h1>')
   .add('button', () => {
@@ -9,4 +11,12 @@ storiesOf('Demo', module)
     button.innerText = 'Hello Button';
     button.addEventListener('click', e => console.log(e));
     return button;
+  })
+  .add('button Green', () => {
+    const buttonGreen = document.createElement('button');
+    buttonGreen.type = 'button';
+    buttonGreen.innerText = 'Hello Button';
+    buttonGreen.addEventListener('click', e => console.log(e));
+    buttonGreen.classList.add('greenButton')
+    return buttonGreen;
   });
