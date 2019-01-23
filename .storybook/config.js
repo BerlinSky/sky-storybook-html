586fb2ef0528cfb6a9882d302d5bea09c4b61544
Berlin Sky <pedroyoung8989@gmail.com>
2019-01-22 16:46:18 -0800

    add viewports

--- .storybook/config.js
+++ .storybook/config.js
@@ -1,5 +1,10 @@
 import { configure, addDecorator} from '@storybook/html';
+
+import { configureViewport } from '@storybook/addon-viewport';
+
 import { checkA11y } from '@storybook/addon-a11y';
+
+configureViewport();
 
 // automatically import all files ending in *.stories.js
 const req = require.context('../stories', true, /.stories.js$/);
